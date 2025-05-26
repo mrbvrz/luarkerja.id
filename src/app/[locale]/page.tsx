b9bec754766d7fd20ed2ed6839f2bb24a8c8818d
Hasan Suryaman <hasan.suryaman@gmail.com>
2025-05-27 05:50:44 +0700

    add languange toogle

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,10 +1,13 @@
 import { useTranslations } from 'next-intl';
 import { Link } from '@/i18n/navigation';
+import LocaleDropdown from '../components/LocaleDropdown';
+import '@/app/globals.css';
 
 export default function HomePage() {
     const t = useTranslations('Index');
     return (
         <div>
+            <LocaleDropdown />
             <h1>{t('title')}</h1>
             <Link href="/about">{t('description')}</Link>
         </div>
