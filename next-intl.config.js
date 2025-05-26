7b5892e7b16e4caafd5af80d5c2d9372ba6b8b8a
Hasan Suryaman <hasan.suryaman@gmail.com>
2025-05-27 05:42:17 +0700

    add prettier and run format

--- next-intl.config.js
+++ next-intl.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	locales: ["en", "id"],
-	defaultLocale: "en",
-	localePrefix: "as-needed", // atau 'always', tergantung preferensi
+    locales: ['en', 'id'],
+    defaultLocale: 'en',
+    localePrefix: 'as-needed' // atau 'always', tergantung preferensi
 };
